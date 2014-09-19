048a55e8585bca42219b1867b8bb5ace2aaf58af
Connor Hindley <conn.hindley@gmail.com>
2014-09-19 10:51:00 -0400

    testing html5 mode

--- client/app/app.js
+++ client/app/app.js
@@ -10,4 +10,6 @@ angular.module('rhlabsangularApp', [
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
+
+    $locationProvider.html5Mode(true);
   });
