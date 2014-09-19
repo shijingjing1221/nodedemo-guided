3f1c5578a33d66c8c0c1de40e9fa54be3f175fb0
Connor Hindley <conn.hindley@gmail.com>
2014-09-18 22:43:37 -0400

    don't overlap the labs gear menu

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,12 +4,15 @@ angular.module('rhlabsangular')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
       'title': 'Home',
-      'link': '#'
+      'link': '#/'
+    }, {
+      'title': 'About',
+      'link': '#/about'
     }];
 
     $scope.isCollapsed = true;
 
     $scope.isActive = function(route) {
-      return route === $location.path();
+      return route === '#' + $location.path();
     };
   });
