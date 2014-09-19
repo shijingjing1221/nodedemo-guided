6f55fc6c32b5f983484af09c7dd3f2a28ec95915
Connor Hindley <conn.hindley@gmail.com>
2014-09-19 11:46:12 -0400

    switch to compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -473,7 +473,7 @@ module.exports = function(grunt) {
             '<%= yeoman.client %>/app',
             '<%= yeoman.client %>/components'
           ],
-          compass: false
+          compass: true
         },
         files: {
           '.tmp/app/app.css': '<%= yeoman.client %>/app/app.scss'
---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'breakpoint'
+
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'client'
+sass_dir = 'client'
+images_dir = 'client/assets'
+javascripts_dir = 'client'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
