4c265f78e8e0e134cd0ebde81ce13dfc8950f4c5
Connor Hindley <conn.hindley@gmail.com>
2014-09-30 14:22:26 -0400

    make loader hacky
    because requirejs

--- client/loader.js
+++ client/loader.js
@@ -1,22 +1,50 @@
 /*global document, window, angular*/
 (function() {
-    window.chrometwo_require([
-        'angular128',
-        'jquery'
-    ], function(angular, jq) {
-        'use strict';
+  window.chrometwo_require([
+    'angular128',
+    'jquery'
+  ], function(angular, jq) {
+    'use strict';
 
-        if (!window.LABS_DEBUG) {
-            window.deps = ['app/vendor.js?bust=0.0.1', 'app/app.js?bust=0.0.1'];
-        }
-
-        window.require.config({
-            baseUrl: '/labs/rhlabsangular'
-        });
-
-        window.chrometwo_require(window.deps, function() {
-            angular.bootstrap(document, ['rhlabsangularApp']);
-            jq('#rhlabsangular').fadeIn();
-        });
+    window.require.config({
+      baseUrl: '/labs/rhlabsangular'
     });
+
+    if (!window.LABS_DEBUG) {
+      window.deps = ['app/vendor.js?bust=0.0.2', 'app/app.js?bust=0.0.2'];
+    }
+    // keep track of deferreds we are loading
+    var dfds = [];
+    var qLoad = function(mod, index) {
+      var previousDfd = dfds[index - 1];
+      dfds[index] = new jq.Deferred();
+      // Internal load that actually wraps the chrometwo_require
+      var _load = function() {
+        window.chrometwo_require(mod.split(), function() {
+          // All good, resolve deferred
+          dfds[index].resolve();
+        });
+      };
+      if (previousDfd) {
+        // We have a previous mod loading, chain the next load
+        previousDfd.then(_load);
+      } else {
+        // First module being loaded. Fire away
+        _load();
+      }
+      return dfds[index].promise();
+    };
+    // Queue up loading of modules
+    for (var i = 0; i < window.deps.length; i++) {
+      qLoad(window.deps[i], i);
+    }
+    // Once all modules have loaded bootstrap it
+    jq.when.apply(jq, dfds).then(function() {
+      // Bootstrap angular app
+      angular.bootstrap(document, ['rhlabsangularApp']);
+      // Fade in main element
+      jq('#rhlabsangular').fadeIn();
+    });
+
+  });
 })();
